0b8fdea03e8797b2e3d38d59f004ea90d8ed7604
aguss002 <agustinacordoba002@gmail.com>
2025-07-16 16:46:42 -0300

    Subir proyecto

---
+++ SistemaGestorDeTurnos/src/user/interaction/GestionDeTurnos.java
@@ -0,0 +1,10 @@
+package user.interaction;
+
+public class GestionDeTurnos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SistemaGestorDeTurnos/test/test/TurnoTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TurnoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
