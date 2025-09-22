c7f3e5131a0a1ba9d93e8cd5131abd20c1797a54
aguss002 <agustinacordoba002@gmail.com>
2025-09-21 22:54:46 -0300

    Modifico el constructor

--- SistemaGestorDeTurnos/src/model/Paciente.java
+++ SistemaGestorDeTurnos/src/model/Paciente.java
@@ -1,5 +1,15 @@
 package model;
 
-public class Paciente {
+import java.time.LocalDate;
+
+
+public class Paciente extends Persona {
+
+	public Paciente(String nombre, String apellido, String dni, String telefono, LocalDate fechaDeNacimiento) {
+		super(nombre, apellido, dni, telefono, fechaDeNacimiento);
+	}
+
+	
+	
 
 }
