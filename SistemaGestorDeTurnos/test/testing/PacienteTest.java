5be96b7fd02669ed02a27e9e9f9060ea9c9d423a
aguss002 <agustinacordoba002@gmail.com>
2025-09-21 22:58:17 -0300

    Realiza tests para verificar el correcto funcionamiento de verificarLongitudDNI() y verificarCaracteresNumericos()

--- SistemaGestorDeTurnos/test/testing/PacienteTest.java
+++ SistemaGestorDeTurnos/test/testing/PacienteTest.java
@@ -1,14 +1,22 @@
 package testing;
 
 import static org.junit.Assert.*;
-
+import java.time.LocalDate;
 import org.junit.Test;
+import model.Paciente;
 
 public class PacienteTest {
-
+	
+	private static Paciente paciente = new Paciente("Lucas", "Martinez", "12345678", "12345678", LocalDate.of(2004, 06, 20));
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void verificoQueElDNICumplaConLaLongitudRequerida() {
+		assertTrue(paciente.verificarLongitudDNI());
+	}
+	
+	@Test
+	public void verificaQueElDNIContengaSoloNumeros() {
+		assertTrue(paciente.verificarCaracteresNumericos());
 	}
 
 }
