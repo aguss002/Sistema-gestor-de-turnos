9bf4a62654419cac1aa2e41878347245e3bc80e9
aguss002 <agustinacordoba002@gmail.com>
2025-11-30 19:33:46 -0300

    Subi el proyecto

---
+++ SistemaGestorDeTurnos/src/model/Persona.java
@@ -0,0 +1,8 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public abstract class Persona {
+	
+}
---
+++ SistemaGestorDeTurnos/testing/test/PacienteTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import model.Paciente;
+
+class PacienteTest {
+
+}
