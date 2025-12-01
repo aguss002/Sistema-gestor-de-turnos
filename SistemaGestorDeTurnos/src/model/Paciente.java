ff57db4f46f7ce4d0c100d4540bc176037d1ed28
aguss002 <agustinacordoba002@gmail.com>
2025-11-30 21:50:19 -0300

    Agrego metodos y implemento la clase abstracta Persona

--- SistemaGestorDeTurnos/src/model/Paciente.java
+++ SistemaGestorDeTurnos/src/model/Paciente.java
@@ -4,6 +4,41 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Paciente {
+public class Paciente extends Persona {
+
+	private String obraSocial;
+	private List<Turno> turnosResevados;
+
+	public Paciente(String nombre, String apellido, String dni, String telefono, LocalDate fechaDeNacimiento) {
+		super(nombre, apellido, dni, telefono, fechaDeNacimiento);
+	}
+
+	public Paciente(String nombre, String apellido, String dni, String telefono, LocalDate fechaDeNacimiento,
+			String obraSocial) {
+		super(nombre, apellido, dni, telefono, fechaDeNacimiento);
+		this.obraSocial = obraSocial;
+		this.turnosResevados = new ArrayList<>();
+	}
+
+	public String getObraSocial() {
+		return obraSocial;
+	}
+
+	public void setObraSocial(String obraSocial) {
+		this.obraSocial = obraSocial;
+	}
+
+	public List<Turno> getTurnosResevados() {
+		return turnosResevados;
+	}
+
+	public void setTurnosResevados(List<Turno> turnosResevados) {
+		this.turnosResevados = turnosResevados;
+	}
+
+	@Override
+	public String toString() {
+		return "\nNombre del paciente: " + nombre + " " + apellido + "\nObra social: " + obraSocial + "\nDNI: " + dni + "\nTurnos reservados: " + turnosResevados;
+	}
 	
 }
