51f8afcad7b8f58e41774c31dbdde3442c5a39fb
aguss002 <agustinacordoba002@gmail.com>
2025-11-30 21:52:44 -0300

    Verifico el correcto funcionamiento del metodo setTelefono()

--- SistemaGestorDeTurnos/testing/test/PacienteTest.java
+++ SistemaGestorDeTurnos/testing/test/PacienteTest.java
@@ -10,4 +10,16 @@ import model.Paciente;
 
 class PacienteTest {
 
+	Paciente paciente = new Paciente("Martin", "Garcia", "12345678", "12347567", LocalDate.of(2001, 6, 15));
+
+	@Test
+	void testTelefonoInvalido() {
+		assertThrows(IllegalArgumentException.class, () -> paciente.setTelefono("123w456"));
+	}
+
+	@Test
+	void testTelefonoValido() {
+		assertDoesNotThrow(() -> paciente.setTelefono("123456789"));
+	}
+
 }
